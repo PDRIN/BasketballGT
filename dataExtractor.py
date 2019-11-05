429635fce2bc12ed9513a4e4a412398615ea5976
Pedro Elias <pedro-elias1997@hotmail.com>
2019-11-05 14:23:01 -0300

    WIP extract accuracy

--- dataExtractor.py
+++ dataExtractor.py
@@ -5,86 +5,81 @@
 # 4 = pointsHome (scored in this event)
 # 5 = team2 (event)
 
-file = open('data/2018/201710170CLE.txt', 'r')
+import os
 
-lines_proccessed = 0
-score = [0,0]
+balancedDir = 'data/splitted/balanced/'
+balancedDirCod = os.fsencode(balancedDir)
 
+for i, file in enumerate(os.listdir(balancedDirCod)):
+	filename = os.fsdecode(file)
 
-at2_attempt = 0
-at2_success = 0
-at3_attempt = 0
-at3_success = 0
+	print(filename)
 
-for i, line in enumerate(file):
+	score = [0,0]
+	at2_attempt = 0
+	at2_success = 0
+	at2_rate = 0
+	at3_attempt = 0
+	at3_success = 0
+	at3_rate = 0
 
-	columns = line.split(',')
+	with open (balancedDir + filename, 'r') as f:
 
-	#game start
-	if i == 0:
-		lines_proccessed += 1
-		team1 = columns[1]
-		team2 = columns[5]
-		print(team1, team2)
-		continue
+		for i, line in enumerate(f):
 
-	#quarter start and jump ball
-	elif columns[1] and (columns[5] and columns[5] != '\n'):
-		lines_proccessed += 1
-		continue
+			columns = line.split(',')
 
-	elif columns[1]:
-		if columns[1].find('misses') > 0:
-			if columns[1].find('2-pt') > 0:
-				at2_attempt += 1
-				lines_proccessed+=1
-			elif columns[1].find('3-pt') > 0:
-				at3_attempt += 1
-				lines_proccessed+=1
-			elif columns[1].find('free') > 0:
-				lines_proccessed+=1
+			#game start
+			if i == 0:
+				team1 = columns[1]
+				team2 = columns[5]
+				print(team1, 'x', team2[:-1])
+				continue
 
-		elif columns[1].find('makes') > 0:
-			if columns[1].find('2-pt') > 0:
-				at2_attempt += 1
-				at2_success += 1
-				lines_proccessed+=1
-				score[0] += 2
-			elif columns[1].find('3-pt') > 0:
-				at3_attempt += 1
-				at3_success += 1
-				lines_proccessed+=1
-				score[0] += 3
-			elif columns[1].find('free') > 0:
-				lines_proccessed+=1
-				score[0] += 1
+			#quarter start and jump ball
+			elif columns[1] and (columns[5] and columns[5] != '\n'):
+				continue
 
-	elif columns[5] and columns[5] != '\n':
+			elif columns[1]:
+				if columns[1].find('misses') > 0:
+					if columns[1].find('2-pt') > 0:
+						at2_attempt += 1
+					elif columns[1].find('3-pt') > 0:
+						at3_attempt += 1
 
-		if columns[5].find('misses') > 0:
-			if columns[5].find('2-pt') > 0:
-				at2_attempt += 1
-				lines_proccessed+=1
-			elif columns[5].find('3-pt') > 0:
-				at3_attempt += 1
-				lines_proccessed+=1
-			elif columns[5].find('free') > 0:
-				lines_proccessed+=1
+				elif columns[1].find('makes') > 0:
+					if columns[1].find('2-pt') > 0:
+						at2_attempt += 1
+						at2_success += 1
+						score[0] += 2
+					elif columns[1].find('3-pt') > 0:
+						at3_attempt += 1
+						at3_success += 1
+						score[0] += 3
+					elif columns[1].find('free') > 0:
+						score[0] += 1
 
-		elif columns[5].find('makes') > 0:
-			if columns[5].find('2-pt') > 0:
-				at2_attempt += 1
-				at2_success += 1
-				lines_proccessed+=1
-				score[1] += 2
-			elif columns[5].find('3-pt') > 0:
-				at3_attempt += 1
-				at3_success += 1
-				lines_proccessed+=1
-				score[1] += 3
-			elif columns[5].find('free') > 0:
-				lines_proccessed+=1
-				score[1] += 1
+			elif columns[5] and columns[5] != '\n':
 
-print('lines_proccessed = ', lines_proccessed)
-print(score, at2_attempt, at2_success, at3_attempt, at3_success)
+				if columns[5].find('misses') > 0:
+					if columns[5].find('2-pt') > 0:
+						at2_attempt += 1
+					elif columns[5].find('3-pt') > 0:
+						at3_attempt += 1
+
+				elif columns[5].find('makes') > 0:
+					if columns[5].find('2-pt') > 0:
+						at2_attempt += 1
+						at2_success += 1
+						score[1] += 2
+					elif columns[5].find('3-pt') > 0:
+						at3_attempt += 1
+						at3_success += 1
+						score[1] += 3
+					elif columns[5].find('free') > 0:
+						score[1] += 1
+
+		at2_rate = at2_success/at2_attempt
+		at3_rate = at3_success/at3_attempt
+
+	print(at2_rate, at3_rate, '\n')
