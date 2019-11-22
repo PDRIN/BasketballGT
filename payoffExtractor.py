b6b78634de5288e0785105bd726d7791918be8ee
Pedro Elias <pedro-elias1997@hotmail.com>
2019-11-22 17:04:40 -0300

    WIP get_payoff, created data structure

--- payoffExtractor.py
+++ payoffExtractor.py
@@ -13,9 +13,12 @@ class Strats(Enum):
 	THREE = 1
 	LANE_THREE = 2
 
-def change_player(comp, player_out, player_in):
-	comp['players'].remove(player_out)
-	comp['players'].append(player_in)
+class Stats():
+	def __init__(self):
+		self.try_2 = 0
+		self.hit_2 = 0
+		self.try_3 = 0
+		self.hit_3 = 0
 
 def remove_noise(string):
 	newString = string
@@ -25,6 +28,16 @@ def remove_noise(string):
 		newString = newString[1:]
 	
 	return newString
+
+def change_player(comp, player_out, player_in):
+	truePlayerOut = remove_noise(player_out)
+	truePlayerIn = remove_noise(player_in)
+
+	print(truePlayerOut, truePlayerIn)
+
+	comp['players'].remove(player_out)
+	comp['players'].append(player_in)
+	#solve_comp(comp)
 
 def add_player_to_comp(comp, player):
 	truePlayer = remove_noise(player)
@@ -69,14 +82,6 @@ def first_read(file, team_comps):
 	team2 = ''
 
 	lines = file.readlines()
-	first_line = lines[0]
-
-	last_line = lines[-1]
-	columns = last_line.split(',')
-	score = columns[3]
-	points = score.split('-')
-
-	victorious = team1 if points[1] <= points[0] else team2
 
 	for i, line in enumerate(lines):
 
@@ -100,6 +105,90 @@ def first_read(file, team_comps):
 
 	print(team_comps)
 
+def extract_payoff(team, victorious, action, team_comps, data):
+	words = action.split(' ')
+
+	#if words[0][0] == '\"':
+		#print('ADLER', action, path)
+
+	if (action.find('misses') > 0) or (action.find('makes') > 0):
+		#soca bagulho no payoff
+
+	elif action.find('enters') > 0:	
+
+		player_out = words[0] + ' ' + words[1]
+
+		if words[1] == 'Mbah':
+			player_in = words[8] + ' ' + words[9]
+			change_player(team_comps[team], player_out, player_in)
+
+		elif words[1] == 'Lemon':
+			player_in = words[7] + ' ' + words[8]
+			change_player(team_comps[team], player_out, player_in)
+
+		else:
+			player_in = words[6] + ' ' + words[7]
+			change_player(team_comps[team], player_out, player_in)
+
+
+def search_team_action(columns, team_comps, data, team1, team2, victorious):
+	#team1 acts
+	if columns[1]:
+		extract_payoff(team1, victorious, columns[1], team_comps, data)
+
+	#team2 acts
+	elif columns[5] and columns[5] != '\n':
+		extract_payoff(team2, victorious, columns[1], team_comps, data)
+
+def second_read(file, team_comps, data):
+	team1 = ''
+	team2 = ''
+
+	lines = file.readlines()
+
+	last_line = lines[-1]
+	columns = last_line.split(',')
+	score = columns[3]
+	points = score.split('-')
+
+	victorious = team1 if points[1] <= points[0] else team2
+
+	for i, line in enumerate(lines):
+
+		columns = line.split(',')
+
+		if i == 0:
+			team1 = columns[1]
+			team2 = columns[5][:-1]
+
+		elif i == 1 or i == 2:
+			continue
+
+		else:
+			search_team_action(columns, team_comps, data, team1, team2, victorious)
+
+def build_data_dic():
+	data = {
+		'even': {
+			'vitoria': [],
+			'derrota': []
+		},
+		'one-sided': {
+			'vitoria': [],
+			'derrota': []
+		}
+	}
+
+	for gameType in data:
+		for result in data[gameType]:
+			for strat1 in Strats:
+				data[gameType][result].append([])
+				for strat2 in Strats:
+					data[gameType][result][strat1.value].append([])
+					data[gameType][result][strat1.value][strat2.value] = Stats()
+
+	return data
+
 ################################################################################
 ################################################################################
 ################################################################################
@@ -115,22 +204,17 @@ oneResultsFile = open('results/oneGames.txt', 'w')
 
 dirs = [evenDir, oneDir]
 
-data = {
-	'even': {
-		'vitoria': {},
-		'derrota': {}
-	},
-	'one-sided': {
-		'vitoria': {},
-		'derrota': {}
-	}
-}
+data = build_data_dic()
 
 for d in dirs:
 
 	cod = os.fsencode(d)
 	
 	for i, file in enumerate(os.listdir(cod)):
+		# if d == evenDir:
+		# 	read_file(path, data['even'])
+		# else:
+		# 	read_file(path, data['one-sided'])
 		
 		filename = os.fsdecode(file)
 
@@ -140,9 +224,9 @@ for d in dirs:
 
 		team_comps = {}
 
+		print(filename)
+
 		first_read(f, team_comps)
 
-		# if d == evenDir:
-		# 	read_file(path, data['even'])
-		# else:
-		# 	read_file(path, data['one-sided'])
+		second_read(f, team_comps, data)
+
--- test.py
+++ test.py
@@ -1,13 +1,38 @@
-a = {
-	'oi': {
-		'a': 1,
-		'b': 2
+from enum import Enum
+class Strats(Enum):
+	LANE = 0
+	THREE = 1
+	LANE_THREE = 2
+
+class Stats():
+	def __init__(self):
+		self.try_2 = 0
+		self.hit_2 = 0
+		self.try_3 = 0
+		self.hit_3 = 0
+
+def build_data_dic():
+	data = {
+		'even': {
+			'vitoria': [],
+			'derrota': []
+		},
+		'one-sided': {
+			'vitoria': [],
+			'derrota': []
+		}
 	}
-}
 
-def dale(di):
-	di['a'] = 7
+	for gameType in data:
+		for result in data[gameType]:
+			for strat1 in Strats:
+				data[gameType][result].append([])
+				for strat2 in Strats:
+					data[gameType][result][strat1.value].append([])
+					data[gameType][result][strat1.value][strat2.value] = Stats()
 
-dale(a['oi'])
+	return data
 
-print(a)
+data = build_data_dic()
+print(data)
+#print(data['even']['vitoria'][0][2].try_2)
