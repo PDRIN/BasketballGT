e90f4d81513adc36de98e77c22521c0daa5c3d33
Pedro Elias <pedro-elias1997@hotmail.com>
2019-11-27 02:39:19 -0300

    think it's done

---
+++ stacked_bars.py
@@ -0,0 +1,94 @@
+import plotly.graph_objects as go
+
+top_labels = ['Lane', '3-points', 'Balanced']
+
+colors = ['rgb(128,155,206)', 'rgb(250,117,117)', 'rgb(189,136,162)']
+
+x_data = [[9, 13.9, 77.1],
+          [18.7, 23.1,58.2]]
+
+y_data = ['Real Usage',
+          'Nash Equi.']
+
+fig = go.Figure()
+
+for i in range(0, len(x_data[0])):
+    for xd, yd in zip(x_data, y_data):
+        fig.add_trace(go.Bar(
+            x=[xd[i]], y=[yd],
+            orientation='h',
+            marker=dict(
+                color=colors[i],
+                line=dict(color='rgb(248, 248, 249)', width=1)
+            )
+        ))
+
+fig.update_layout(
+    xaxis=dict(
+        showgrid=False,
+        showline=False,
+        showticklabels=False,
+        zeroline=False,
+        domain=[0.15, 1]
+    ),
+    yaxis=dict(
+        showgrid=False,
+        showline=False,
+        showticklabels=False,
+        zeroline=False,
+    ),
+    barmode='stack',
+    paper_bgcolor='rgb(248, 248, 255)',
+    plot_bgcolor='rgb(248, 248, 255)',
+    margin=dict(l=300, r=300, t=600, b=0),
+    showlegend=False,
+)
+
+annotations = []
+
+for yd, xd in zip(y_data, x_data):
+    # labeling the y-axis
+    annotations.append(dict(xref='paper', yref='y',
+                            x=0.14, y=yd,
+                            xanchor='right',
+                            text=str(yd),
+                            font=dict(family='Arial', size=35,
+                                      color='rgb(30,30,30)'),
+                            showarrow=False, align='right'))
+    # labeling the first percentage of each bar (x_axis)
+    annotations.append(dict(xref='x', yref='y',
+                            x=xd[0] / 2, y=yd,
+                            text=str(xd[0]) + '%',
+                            font=dict(family='Arial', size=35,
+                                      color='rgb(30,30,30)'),
+                            showarrow=False))
+    # labeling the first Likert scale (on the top)
+    if yd == y_data[-1]:
+        annotations.append(dict(xref='x', yref='paper',
+                                x=xd[0] / 2, y=1.1,
+                                text=top_labels[0],
+                                font=dict(family='Arial', size=35,
+                                          color='rgb(30,30,30)'),
+                                showarrow=False))
+    space = xd[0]
+    for i in range(1, len(xd)):
+            # labeling the rest of percentages for each bar (x_axis)
+            annotations.append(dict(xref='x', yref='y',
+                                    x=space + (xd[i]/2), y=yd,
+                                    text=str(xd[i]) + '%',
+                                    font=dict(family='Arial', size=35,
+                                              color='rgb(30, 30, 30)'),
+                                    showarrow=False))
+            # labeling the Likert scale
+            if yd == y_data[-1]:
+                annotations.append(dict(xref='x', yref='paper',
+                                        x=space + (xd[i]/2), y=1.1,
+                                        text=top_labels[i],
+                                        font=dict(family='Arial', size=35,
+                                                  color='rgb(67, 67, 67)'),
+                                        showarrow=False))
+            space += xd[i]
+
+fig.update_layout(annotations=annotations)
+
+fig.show()
